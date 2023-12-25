f715ec19dea4453b6906b261da5c1f56766fc443
Alexander Held <alexander.held@cern.ch>
2023-12-25 15:34:07 +0100

    feat: add template

---
+++ template/puzzle.py
@@ -0,0 +1,19 @@
+import fileinput
+
+import heapq
+import random
+import re
+import statistics
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cache, cmp_to_key, reduce
+from itertools import accumulate, combinations, count, cycle, permutations, product
+from math import inf, lcm, prod
+from operator import eq, gt, lt
+
+import numpy as np
+
+x = [line for line in fileinput.input()]
+
+print(f"part 1: {None}")
+print(f"part 2: {None}")
